16535f8a7ca3071f4627160d031af8b5e39929e2
Everlyn <gachuieve32@gmail.com>
2023-09-16 18:09:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+const event = "birthday";
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  const messages = writeCards(names, event);
+  console.log(messages);
+  
+  function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
+  countDown(10);
+  
